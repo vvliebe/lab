36131ba1f8dd4c8367329ac9ea437288b0b94f15
vvliebe <vvliebe@gmail.com>
2017-06-29 04:05:36 +0800

    add koa-jwt

--- admin/app.ts
+++ admin/app.ts
@@ -2,6 +2,8 @@ import * as Koa from 'koa'
 import * as bodyParser from 'koa-bodyparser'
 import * as cors from 'koa2-cors'
 import * as logger from 'koa-logger'
+import * as jwt from 'koa-jwt'
+
 // import * as KoaSwagger from 'koa2-swagger-ui'
 
 import { buildRoutes } from './router'
@@ -11,6 +13,7 @@ import error from './middlewares/error'
 const startup = () => {
   connectDB()
   const app = new Koa()
+  app.use(jwt({secret: 'vvliebe'}).unless({ path: [/^\/user/]}))
   app.use(cors())
   app.use(bodyParser())
   app.use(logger())
